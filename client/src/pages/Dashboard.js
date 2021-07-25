128c55dc14abff407bf69b95bdd442ae9f858d72
Belle Duran <bduran04@gmail.com>
2021-07-25 15:52:42 -0500

    dashboard WIP

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,5 +1,5 @@
 import { Button } from '@material-ui/core';
-import React, { useContext, useEffect } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { useHistory } from "react-router-dom";
 import AuthContext from '../contexts/AuthContext';
 
@@ -7,15 +7,27 @@ import API from "../utils/api"
 
 //create button that routes to the problems page 
 const Dashboard = () => {
-  
-const currentUser = API.currentUser()
-console.log(currentUser)
+
+const [userData, setUserData] = useState([]);
+
+useEffect(() => {
+    API.currentUser()
+    .then(res=>{
+        console.log(res)
+        
+    })
+    .catch(err=>{
+        console.log(err);
+    })
+  }, []);
+
 
     return (
         <div>
-            <h1 textAlign="center">
+            <h1 justifyContent="center">
             Dashboard Page
             </h1>
+
              {/* <Button variant="contained" onClick={() => {history.push('/?equation=${equation}')}}>Back</Button> */}
         </div>
     )
